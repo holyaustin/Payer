f8e290db431d6d1a406b48d610466f71ebf09af2
holyaustin <holyaustin@yahoo.com>
2023-10-24 11:22:12 +0100

    initail commit

---
+++ utils/chain/index.ts
@@ -0,0 +1,13 @@
+export enum CHAINS_IDS {
+    ETHEREUM = "ethereum",
+    BSC = "bsc",
+    POLYGON = "polygon",
+    BASE = "base-mainnet",
+    BASEGOERLI = "base-testnet",
+    OPTIMISM = "optimism",
+    CUSTOM = "custom_evm",
+}
+
+export enum CHAINS_ENUMS {
+    ETHEREUM = "Ethereum",
+}
